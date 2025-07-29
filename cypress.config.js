ff70658e2b8268b8fe26b84bf2dc302a10f7a88c
QATJTeam <rajkumardalwai@tractorjunction.com>
2025-07-29 12:20:11 +0530

    Installed Allure Plug-in & developed Jenkinsfile

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
 
@@ -19,6 +20,8 @@ e2e: {
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      allureWriter(on, config);
+      return config;
     },
 
   specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
